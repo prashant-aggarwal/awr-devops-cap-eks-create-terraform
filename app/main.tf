87063801c1ed32a52ca6c5270e3c7ffe6a4b75ee
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-07-14 22:43:06 +0530

    Prashant - Adjusted providers

--- app/main.tf
+++ app/main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 # Data sources
 data "aws_availability_zones" "available" {
   filter {
--- app/providers.tf
+++ app/providers.tf
@@ -1,12 +1,21 @@
-provider "aws" {
-  region = var.region
-}
-
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
