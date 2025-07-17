87676f4c1f4c21c78446d33bcff5f184df812fa4
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-07-17 21:45:03 +0530

    Prashant - Added separate aws_eks_access_policy_association

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -104,11 +104,16 @@ data "aws_caller_identity" "current" {}
 resource "aws_eks_access_entry" "api_access_entry" {
   cluster_name      = var.cluster_name
   principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/EKSServiceDeploymentRole"
+}
 
-  policy_associations {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    access_scope {
-      type       = "cluster"
-    }
+resource "aws_eks_access_policy_association" "example" {
+  cluster_name  = var.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_eks_access_entry.api_access_entry.principal_arn
+
+  access_scope {
+    type       = "cluster"
   }
+
+  depends_on = [aws_eks_access_entry.api_access_entry]
 }
