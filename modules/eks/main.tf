d873204d6248addb41e6572150d374ce14ef9a77
Prashant Aggarwal <prashant.aggarwal05@gmail.com>
2025-07-17 21:14:55 +0530

    Prashant - Added API authentication mode and access entry

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -52,6 +52,10 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = var.cluster_version
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = true
@@ -91,5 +95,20 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   tags = {
     Name = "${var.cluster_name}-eks-irsa"
+  }
+}
+
+# Access entry to allow API calls
+data "aws_caller_identity" "current" {}
+
+resource "aws_eks_access_entry" "api_access_entry" {
+  cluster_name      = var.cluster_name
+  principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/EKSServiceDeploymentRole"
+
+  policy_associations {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    access_scope {
+      type       = "cluster"
+    }
   }
 }
